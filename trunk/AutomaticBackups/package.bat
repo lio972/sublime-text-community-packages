21a20c745bd1002f6bafa465e398497e339e3ad0
mindfiresoftware <mindfiresoftware@159fff5a-be49-0410-a0ae-73795322737e>
2008-03-31 23:03:02 +0000

    Removed file/folder

--- trunk/AutomaticBackups/package.bat
+++
@@ -1,6 +0,0 @@
-IF EXIST AutomaticBackups.zip THEN del AutomaticBackups.zip
-IF EXIST AutomaticBackups.sublime-package THEN del AutomaticBackups.sublime-package
-
-"%ProgramFiles%\7-zip\7z.exe" A AutomaticBackups.zip @listOfFilesInPackage.txt
-
-ren AutomaticBackups.zip AutomaticBackups.sublime-package
