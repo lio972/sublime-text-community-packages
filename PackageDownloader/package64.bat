4d11cf18415a128734cf288c7090528603b15efb
mindfiresoftware <mindfiresoftware@159fff5a-be49-0410-a0ae-73795322737e>
2008-03-31 23:02:49 +0000

    Removed file/folder

--- PackageDownloader/package64.bat
+++
@@ -1,8 +0,0 @@
-@ECHO OFF
-
-del PackageDownloader.sublime-package
-
-set sevenzip="%ProgramFiles(x86)%\7-zip\7z.exe"
-%sevenzip% A -tzip PackageDownloader.sublime-package @listOfFilesInPackage.txt
-
-rem ren PackageDownloader.zip PackageDownloader.sublime-package
