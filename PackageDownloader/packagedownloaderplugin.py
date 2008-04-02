1c2fdab6782b48da0ba04f8e1542a55edbdd3071
mindfiresoftware <mindfiresoftware@159fff5a-be49-0410-a0ae-73795322737e>
2008-04-02 00:00:29 +0000

    updated to use sublime.messageBox over ctypes MessageBox

--- PackageDownloader/packagedownloaderplugin.py
+++ PackageDownloader/packagedownloaderplugin.py
@@ -1,12 +1,5 @@
 import sublime, sublimeplugin, packagedownloader
 
-# import stuff to allow messageboxes
-from ctypes import c_int, WINFUNCTYPE, windll
-from ctypes.wintypes import HWND, LPCSTR, UINT
-prototype = WINFUNCTYPE(c_int, HWND, LPCSTR, LPCSTR, UINT)
-paramflags = (1, "hwnd", 0), (1, "text", "Hi"), (1, "caption", None), (1, "flags", 0)
-MessageBox = prototype(("MessageBoxA", windll.user32), paramflags)
-
 class DownloadPackagesOnSublimeTextWikiCommand(sublimeplugin.ApplicationCommand):
   
   name = "Package Downloader"
@@ -14,6 +7,6 @@ class DownloadPackagesOnSublimeTextWikiCommand(sublimeplugin.ApplicationCommand)
   def run(self, args):
     packageList = packagedownloader.downloadPackages()
     if len(packageList) == 0:
-      MessageBox(text="No Packages Downloaded", caption=self.name)
+      sublime.messageBox("No Packages Downloaded")
     else:
-      MessageBox(text="Downloaded these packages;\n\n  " + str.join("\n  ", packageList) + "\n\nPackages will be installed next time you start Sublime Text.", caption=self.name)
+      sublime.messageBox("Downloaded these packages;\n\n  " + str.join("\n  ", packageList) + "\n\nPackages will be installed next time you start Sublime Text.")
