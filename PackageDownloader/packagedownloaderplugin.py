048b8c6770e551a05de68c38f0a19ec382cfdea0
ndudfield <ndudfield@159fff5a-be49-0410-a0ae-73795322737e>
2008-04-20 12:35:16 +0000

    Updated packagedownloaderplugin.py to download packages in another thread, leaving Sublime unblocked.

--- PackageDownloader/packagedownloaderplugin.py
+++ PackageDownloader/packagedownloaderplugin.py
@@ -1,12 +1,26 @@
-import sublime, sublimeplugin, packagedownloader
+import sublime, sublimeplugin, packagedownloader, threading
+from functools import partial
 
 class DownloadPackagesOnSublimeTextWikiCommand(sublimeplugin.ApplicationCommand):
-  
   name = "Package Downloader"
   
-  def run(self, args):
-    packageList = packagedownloader.downloadPackages()
+  def getPackages(self):
+    packageList = packagedownloader.downloadPackages()    
+    sublime.setTimeout(partial(self.notify, packageList), 1)
+    
+  def notify(self, packageList):
     if len(packageList) == 0:
-      sublime.messageBox("No Packages Downloaded")
-    else:
-      sublime.messageBox("Downloaded these packages;\n\n  " + str.join("\n  ", packageList) + "\n\nPackages will be installed next time you start Sublime Text.")
+      msg = "No Packages Downloaded"
+    
+    else:  
+      packages = "\n".join(packageList)
+      msg = "Downloaded these packages;\n\n  " + packages +\
+             "\n\nPackages will be installed next time you start Sublime Text."
+             
+    sublime.messageBox(msg)
+    
+  def run(self, args):
+    sublime.statusMessage("Downloading Packages")
+    t = threading.Thread(target=self.getPackages)
+    t.start()
+    
