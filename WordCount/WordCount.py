4844c900623ee4f5e753eef1421b1d0d1a3d95b9
mindfiresoftware <mindfiresoftware@159fff5a-be49-0410-a0ae-73795322737e>
2008-05-09 16:26:57 +0000

    commenting.

--- WordCount/WordCount.py
+++ WordCount/WordCount.py
@@ -6,16 +6,21 @@ class wordCount(sublimeplugin.TextCommand):
 		sublime.statusMessage("Word Count: %s in %s" % (self.count(content), view.fileName()))
 		
 	def count(self, content):
-		# compress all whitespace to single spaces
+		"""counts by counting all the start-of-word characters"""
+	
+		# regex to find word characters
 		wrdRx = re.compile("\w")
 		matchingWrd = False
 		words = 0;
 		for ch in content:
+			# test if this char is a word char
 			isWrd = wrdRx.match(ch) != None
+			
 			if isWrd and not matchingWrd:
 				# we're moving into a word from not-a-word
 				words = words + 1
 				matchingWrd = True
 			if not isWrd:
+				# go back to not matching words
 				matchingWrd = False
 		return words
