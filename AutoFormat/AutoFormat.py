f9da7d4a89976663aae72a55805000d8c8c1a7e1
vimperator <vimperator@159fff5a-be49-0410-a0ae-73795322737e>
2008-09-29 07:01:03 +0000

    replcaed exe with dll

--- AutoFormat/AutoFormat.py
+++ AutoFormat/AutoFormat.py
@@ -1,36 +1,49 @@
 import sublime, sublimeplugin
-import os
-import sys
 import re
+from ctypes import *
 
-import functools
+malloc = cdll.msvcrt.malloc
+malloc.restype = POINTER(c_double)
+free = cdll.msvcrt.free
 
+# // astyle ASTYLE_LIB declarations
+# typedef void (STDCALL *fpError)(int, char*);       // pointer to callback error handler
+# typedef char* (STDCALL *fpAlloc)(unsigned long);   // pointer to callback memory allocation
+# extern "C" EXPORT char* STDCALL AStyleMain(const char*, const char*, fpError, fpAlloc);
+# extern "C" EXPORT const char* STDCALL AStyleGetVersion (void);
+
+# AStyleMain (addr)0x1000aac0 (rel addr)0x0000aac0 (ordinal)2
+
+def ErrorHandler(size, str):
+	print "[callback error handler] size:%d str:%s" % (size, str)
+
+ErrorHandlerCallback = WINFUNCTYPE(None, c_int, c_char_p)
+ERROR_HANDLER = ErrorHandlerCallback(ErrorHandler)
+
+allocated = []
+def MemoryAllocation(size):
+   	arr_type = c_double * size
+   	x = arr_type()
+   	allocated.append(x)
+   	ptr = addressof(x)
+	#print "[callback memory allocation] size:%d ptr:%x" % (size, ptr)   
+   	return ptr	
+	
+MemoryAllocationCallback = WINFUNCTYPE(c_char_p, c_ulong) 
+MEMORY_ALLOCATION = MemoryAllocationCallback(MemoryAllocation)
 
 class AutoFormatCommand(sublimeplugin.TextCommand):
 	def run(self, view, args):
-		f = view.fileName();
-		exe = sublime.packagesPath()
-		exe = os.path.join(exe, "User")
-		exe = os.path.join(exe, "AStyle.exe")
-			
-		# Spaces ion path workaround:
-		# add an extra quote (!) before the quoted command name:
-		# result = os.system('""pythonbugtest.exe" "test"')
-		# Explanation:
-		# there was a time when the cmd prompt treated all spaces as delimiters, so
-		# >cd My Documents
-		# would fail. Nowadays you can do that successfully and even
-		# >cd My Documents\My Pictures
-		# works.
-		# In the old days, if a directory had a space, you had to enclose it in quotes
-		# >cd "My Documents"
-		# But you didn't actually need to include the trailing quote, so you could get away with
-		# >cd "My Documents
-		cmd = '""%(exe)s" "%(args)s""' % {'exe' : exe, 'args' : f } # stderr > out.txt 2>&1
-		result = os.system(cmd)
+		# if the function name has a @ followed by number it probably uses the stdcall calling convention. 
+		libc = windll.AStyle
+		astyle_main = libc[2]
+		astyle_main.restype = c_char_p
+		astyle_main.argtypes = [c_char_p, c_char_p, ErrorHandlerCallback, MemoryAllocationCallback]
 		
-		if not result:		
-			sublime.setTimeout(functools.partial(view.runCommand, 'revert'), 0)
+		region = sublime.Region(0L, view.size())
+		select_all = view.substr(region)
+		pretty_code = astyle_main(select_all, "", ERROR_HANDLER, MEMORY_ALLOCATION)
+		view.replace(region, pretty_code)
 		
 	def isEnabled(self, view, args):
 		lang = re.search(".*/([^/]*)\.tmLanguage$", view.options().getString("syntax")).group(1)
