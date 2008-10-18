dec9d9f7408cdfaa9e23278734628ef13d5a68b5
ndudfield <ndudfield@159fff5a-be49-0410-a0ae-73795322737e>
2008-10-18 12:40:12 +0000

    refactorx

--- CTags/helpers.py
+++ CTags/helpers.py
@@ -2,17 +2,17 @@
 
 import time
 
-def time_function(times=1):
-    def decorator(f):
-        def wrapper(*args, **kw):
-            total_time = 0
-            for t in xrange(times):
-                t = time.time()
-                result = f(*args, **kw)
-                total_time += time.time() - t
-            print f.__name__, total_time / times
-            return result
-        return wrapper
-    return decorator
+def time_function(f):
+    def wrapper(*args, **kw):
+        t = time.time()
+
+        result = f(*args, **kw)
+        
+        t = time.time() -t
+
+        print args[0].__class__.__name__, f.__name__, t
+        
+        return result
+    return wrapper
 
 ################################################################################
