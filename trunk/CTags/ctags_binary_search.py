5f654ede83e79c6408bcf8ee9cd191e4c8dfa74e
ndudfield <ndudfield@159fff5a-be49-0410-a0ae-73795322737e>
2008-10-18 01:19:11 +0000

    fixed circual imports, stopped plugin being enabled

--- trunk/CTags/ctags_binary_search.py
+++ trunk/CTags/ctags_binary_search.py
@@ -1,77 +1,65 @@
 ################################################################################
 
 # Std Libs
-from __future__ import with_statement
+# from __future__ import with_statement
 
-import re
-import bisect
-import os
-import time
+# import re
+# import bisect
+# import os
+# import time
 
-import pprint
-import ctags
+# import pprint
+# import ctags
 
-from helpers import time_function
+# from helpers import time_function
 
 ################################################################################
     
-def log_divides(f):
-    f.accessed = 0
-    def wrapped(self, i):
-        item = f(self, i)
-        f.accessed += 1
-        print f.accessed, i
-        return item
-    return wrapped
+# def log_divides(f):
+#     f.accessed = 0
+#     def wrapped(self, i):
+#         item = f(self, i)
+#         f.accessed += 1
+#         print f.accessed, i
+#         return item
+#     return wrapped
     
-SYMBOL = 0
-FILENAME = 1
+# SYMBOL = 0
+# FILENAME = 1
 
-class TagFile(object):
-    def __init__(self, p, column):
-        self.p = p
-        self.column = column
+# class TagFile(object):
+#     def __init__(self, p, column):
+#         self.p = p
+#         self.column = column
     
-    # @log_divides
-    def __getitem__(self, index):
-        self.fh.seek(index)
-        self.fh.readline()
-        return self.fh.readline().split('\t')[self.column]
+#     @log_divides
+#     def __getitem__(self, index):
+#         self.fh.seek(index)
+#         self.fh.readline()
+#         return self.fh.readline().split('\t')[self.column]
 
-    def __len__(self):
-        return os.stat(self.p)[6]
+#     def __len__(self):
+#         return os.stat(self.p)[6]
 
-    def get(self, tag):
-        with open(self.p) as self.fh:
-            b4 = bisect.bisect_left(self, tag)
-            self.fh.seek(b4)
+#     def get(self, tag):
+#         with open(self.p) as self.fh:
+#             b4 = bisect.bisect_left(self, tag)
+#             self.fh.seek(b4)
 
-            for l in self.fh:
-                comp = cmp(l.split('\t')[self.column], tag)
+#             for l in self.fh:
+#                 comp = cmp(l.split('\t')[self.column], tag)
 
-                if   comp == -1: continue
-                elif comp ==  1: break
+#                 if   comp == -1: continue
+#                 elif comp ==  1: break
                 
-                yield l                
+#                 yield l                
 
-    def get_tags_dict(self, tag):
-        return ctags.parse_tag_lines(self.get(tag))
+#     def get_tags_dict(self, tag):
+#         return ctags.parse_tag_lines(self.get(tag))
 
 ################################################################################
 
-# @time_function(times=1)
-def resort_ctags(tag_file):
-    keys = {}
-    
-    with open(tag_file) as fh:
-        for l in fh:
-            keys.setdefault(l.split('\t')[FILENAME], []).append(l)
-        
-    with open(tag_file + '_unsorted', 'w') as fw:
-        for k in sorted(keys):
-            fw.write(''.join(keys[k]))
-
-if __name__ == '__main__':
-    resort_ctags('C://python25//lib/tags')
+# if __name__ == '__main__':
+    # pass
     
 ################################################################################
