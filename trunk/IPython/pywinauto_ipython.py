e1bf33bb52a7b3429fc8ffdee0479685f9b7e0bd
ndudfield <ndudfield@159fff5a-be49-0410-a0ae-73795322737e>
2009-03-26 04:45:53 +0000

    behavioural enhancements

--- trunk/IPython/pywinauto_ipython.py
+++ trunk/IPython/pywinauto_ipython.py
@@ -1,4 +1,6 @@
 from pywinauto.application import Application
+from subprocess import list2cmdline
+from os.path import join
 
 import time
 
@@ -6,6 +8,12 @@ import time
   u'C:\\python25\\python.exe', u'CaptionButton_Floating_Window',
   u'C:\\python25\\python.exeConsoleWindowClass', u'ConsoleWindowClass']
 
-def launch_ipython():
-    app = Application.start('python C:/python25/scripts/ipython.py D:/ipython/completion_server.py')
+def launch_ipython(completion_server):
+    cmd_list = [
+        'python', 
+        'C:/python25/scripts/ipython.py',
+        join(completion_server, 'completion_server.py')
+    ]
+
+    app = Application.start(list2cmdline(cmd_list))
     return app['ConsoleWindowClass']
