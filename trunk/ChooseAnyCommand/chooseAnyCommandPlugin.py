f8dd72095f111edaed2f8a73abb0caca6bf2e8c5
ndudfield <ndudfield@159fff5a-be49-0410-a0ae-73795322737e>
2009-04-13 12:02:30 +0000

    bug fix; refactor to tuple sort

--- trunk/ChooseAnyCommand/chooseAnyCommandPlugin.py
+++ trunk/ChooseAnyCommand/chooseAnyCommandPlugin.py
@@ -3,57 +3,35 @@
 # 
 import sublime, sublimeplugin, os
 
-#
-# Provides the quickpanel entry for the command;
-#
-# commands = the dictionary of commands (text, window, app)
-# cmd = the name of the command
-# typ = a string representing the command type
-#
-def cmdinfo(commands, cmd, typ):
-  return typ + "\\" + commands[cmd].__module__ + "\\" + cmd # + " " + " - ".join(dir(cmd))
+import inspect, sys
 
-#
-# shows the QuickPanel with every runnable command available to Sublime Text.
-#
+def cmd_runners(window):
+  return  dict (
+    text         = window.activeView(),
+    window       = window,
+    application  = sublime
+  )
+
 class ChooseAnyCommandCommand(sublimeplugin.WindowCommand):
-  
   def run(self, window, args):
-    
-    textCommandNames = sublimeplugin.textCommands.keys()
-    windowCommandNames = sublimeplugin.windowCommands.keys()
-    appCommandNames = sublimeplugin.applicationCommands.keys()
+    to_sort = []
 
-    textCommandNames.sort()
-    windowCommandNames.sort()
-    appCommandNames.sort()
-    
-    textCommands = [cmd for cmd in textCommandNames if window.activeView().canRunCommand(cmd)]
-    windowCommands = [cmd for cmd in windowCommandNames if window.canRunCommand(cmd)]
-    appCommands = [cmd for cmd in appCommandNames if sublime.canRunCommand(cmd)] 
+    for cmd_type, runner in cmd_runners(window).items():
+      cmds_of_type = getattr(sublimeplugin, '%sCommands' % cmd_type)
+      is_enabled = getattr(runner, 'canRunCommand', None)
+      if not is_enabled: continue
+      for cmd in cmds_of_type:
+        if is_enabled(cmd):
+          to_sort.append((cmd_type, cmds_of_type[cmd].__module__, cmd))
 
-    textNames = ["text\\" + cmd for cmd in textCommands]
-    appNames = ["application\\" + cmd for cmd in appCommands]
-    wndNames = ["window\\" + cmd for cmd in windowCommands]
-    
-    displayTextNames = [cmdinfo(sublimeplugin.textCommands, cmd, "text") for cmd in textCommands]
-    displayAppNames = [cmdinfo(sublimeplugin.applicationCommands, cmd, "application") for cmd in appCommands]
-    displayWndNames = [cmdinfo(sublimeplugin.windowCommands, cmd, "window") for cmd in windowCommands]
-    
-    display = displayTextNames + displayAppNames + displayWndNames
-    cmds = textNames + appNames + wndNames
-    
-    window.showQuickPanel("", "executeNamedCommand", cmds, display, sublime.QUICK_PANEL_FILES)
-    
+    to_sort.sort()
+
+    window.showQuickPanel("", "executeNamedCommand", 
+        [`(c[0], c[2])` for c in to_sort],
+        ['\\'.join(c) for c in to_sort],
+        sublime.QUICK_PANEL_FILES )
+
 class ExecuteNamedCommandCommand(sublimeplugin.WindowCommand):
-  def run(self, window, args):    
-    typeAndCmd = args[0].split("\\")
-    typ = typeAndCmd[0]
-    cmd = typeAndCmd[1]
-    
-    if typ=="text":
-      window.activeView().runCommand(cmd)
-    elif typ=="application":
-      sublime.runCommand(cmd)
-    elif typ=="window":
-      window.runCommand(cmd)
+  def run(self, window, args):
+    cmd_type, cmd = eval(args[0])
+    cmd_runners(window)[cmd_type].runCommand(cmd)
