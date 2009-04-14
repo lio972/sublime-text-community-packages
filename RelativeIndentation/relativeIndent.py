f2c7db93cd805351c5be1bef4e23eff57292df36
ndudfield <ndudfield@159fff5a-be49-0410-a0ae-73795322737e>
2009-04-14 00:59:46 +0000

    doesn't use insertInlineSnippet; more consistent

--- RelativeIndentation/relativeIndent.py
+++ RelativeIndentation/relativeIndent.py
@@ -2,6 +2,8 @@
 
 import sublime, sublimeplugin, re, os
 
+import textwrap
+
 ############################### COMMON FUNCTIONS ###############################
 
 def stripPreceding(selection, padding="", rstrip=True):
@@ -14,8 +16,9 @@ def stripPreceding(selection, padding="", rstrip=True):
     for line in (l for l in split if l.strip()):
         for match in preceding_whitespace.finditer(line):
             common_start = min(match.span(1)[1], common_start)
+            if common_start == 0: break
 
-    stripped = "\n".join( [split[0][common_start:]]                      +\
+    stripped = "\n".join( [split[0][common_start:]]  +
                        [padding + l[common_start:] for l in split[1:]] )
     
     return  stripped.rstrip("\n") if rstrip else stripped
@@ -65,7 +68,7 @@ def getTab(view):
 def substrStripPrecedingCommonSpace(view, region, padSecondary=""):
     """
     
-    takes a view, and a Region of it, strips preceding common space 
+    Takes a view, and a Region of it, strips preceding common space 
     so only relative indentation remains
     
     """
@@ -79,29 +82,66 @@ def eraseSelectionLines(view):
     "Erases any line with any selection, even if empty"
     for sel in view.sel(): view.erase(view.fullLine(sel))
 
+def insertOrReplace(view, region, string):
+    if region:
+        view.replace(region, string)
+    else:
+        view.insert(region.begin(), string)
+
+def handleTabs(view, string):
+    if not view.options().get('translateTabsToSpaces'):
+        string = string.replace(getTab(view), '\t')
+
+    return string
+
 ################################ PLUGIN COMMANDS ###############################
+
+def move_cursor_down(view, cursor_pt, cursor_abs):
+    view.sel().clear()
+
+    next_line_starts = view.fullLine(sublime.Region(cursor_abs, cursor_abs)).end()
+    next_line = view.line(sublime.Region(next_line_starts, next_line_starts))
+
+    if len(next_line) < cursor_pt:
+        padding = (cursor_pt - len(next_line)) * ' '
+        view.insert(next_line.end(), handleTabs(view, padding))
+
+    new_pt = (next_line.begin() + cursor_pt)
+    view.sel().add(sublime.Region(new_pt, new_pt  ))
 
 class RelativeIndentCommand(sublimeplugin.TextCommand):
     def run(self, view, args):
-        if args[0] == 'paste':
-            
-            for sel in view.sel():
-                line = view.line(sel)
-                if view.substr(line).isspace():
-                    view.erase(sublime.Region(sel.end(), line.end()))
+        sels = view.sel()  
 
-            selection = sublime.getClipboard()         #.replace("\r\n", "\n")
-            selection = stripPreceding(selection)
-            
-            view.runCommand('insertInlineSnippet', ['$PARAM1', selection])
+        if args[0] == 'paste':
+            clip = textwrap.dedent(sublime.getClipboard()).rstrip().split(u'\n')
+            n = len(clip)  
+
+            if len(sels) > 1:   # Columnar Paste
+                for region in view.sel():
+                    insertOrReplace(view, region, clip.pop(0))
+            else:
+                for i, l in enumerate(clip):
+                    last_line = i+1 == n    
+
+                    sel =  view.sel()[0]               
+                    line = view.line(sel)   
+                    if not last_line and view.substr(line).isspace():
+                        view.runCommand('insertAndDecodeCharacters', ['\n'])
+
+                    cursor_at = sel.end() - line.begin()
+                    insertOrReplace(view, sel, l)
+
+                    if not last_line:
+                        move_cursor_down(view, cursor_at, sel.end())
         else:
-            view.runCommand('expandSelectionTo line')
+            for sel in view.sel(): view.sel().add(view.line(sel))
             view.runCommand(args[0])
 
 class ParamPerSelectionSnippetCommand(sublimeplugin.TextCommand):
     def run(self, view, args):
         selections = []
-        selSet = view.sel()
+        selSet = view.sel()    
         sel1 = selSet[0]
 
         for sel in selSet:
@@ -109,7 +149,7 @@ class ParamPerSelectionSnippetCommand(sublimeplugin.TextCommand):
 
         start, end, displace = linesGetFirstsDisplacement( view, sel1) 
         
-        eraseSelectionLines(view)
+        eraseSelectionLines(view)               
         selSet.clear()
         
         view.insert(start, (displace * ' ') + '\n')
@@ -118,7 +158,7 @@ class ParamPerSelectionSnippetCommand(sublimeplugin.TextCommand):
         selSet.add(sublime.Region(putCursorAt, putCursorAt))
         
         view.runCommand('insertSnippet', args + selections)
-        
+
 class RelativeIndentSnippetCommand(sublimeplugin.TextCommand):
     def run(self, view, args):
         selSet = view.sel()
@@ -130,8 +170,8 @@ class RelativeIndentSnippetCommand(sublimeplugin.TextCommand):
 
             selSet.subtract(sel)
             selSet.add(modifiedRegion)
-            view.replace(modifiedRegion, selectionStripped)
-
+            view.replace(modifiedRegion, selectionStripped) 
+            
         view.runCommand('insertSnippet', args)
-        
+
 ################################################################################
