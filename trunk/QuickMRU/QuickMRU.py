84fe01e6fab151f400e7251fe3abdeb2d3b9db0d
Sami.Pirbay <Sami.Pirbay@159fff5a-be49-0410-a0ae-73795322737e>
2009-04-25 21:30:34 +0000

    Corrected a bug that added an empty entry to the DB when closing a buffer that has no file name.
    New feature: files that are currently unavailable filtered out of the quick panel, but they are kept in the DB

--- trunk/QuickMRU/QuickMRU.py
+++ trunk/QuickMRU/QuickMRU.py
@@ -8,9 +8,9 @@ class QuickMRUCommand(sublimeplugin.WindowCommand):
 	"""
 	Shows a quick panel with the most recently closed files and the	files closed most often.
 	"""
-	
+
 	filename = sublime.packagesPath() + "/QuickMRU/QuickMRU.db" # is there a better way to get the plugin's folder?
-	
+
 	def getDB(self):
 		try:
 			myfile = open(self.filename, "r")
@@ -19,21 +19,26 @@ class QuickMRUCommand(sublimeplugin.WindowCommand):
 		except IOError:
 			db = {}
 		return db
-	
+
 	def trimDB(self):
 		db = self.getDB()
 		latest       = sorted(db.keys(), key=lambda x:db[x]['date'   ], reverse=True)
 		mostFrequent = sorted(db.keys(), key=lambda x:db[x]['counter'], reverse=True)
 		files = set(mostFrequent[:100] + latest[:100])
-		newDB = {}			
+		newDB = {}
 		for x in files:
 			newDB[x]=db[x]
-		
+
+		del newDB[None]
+
 		myfile = open(self.filename, "w")
 		pickle.dump(newDB, myfile)
 		myfile.close()
-		
+
 	def onClose(self, view):
+		if not view.fileName():
+			return
+
 		db = self.getDB();
 
 		now = datetime.datetime.today();
@@ -42,41 +47,44 @@ class QuickMRUCommand(sublimeplugin.WindowCommand):
 			db[view.fileName()]['date'] = now
 		else:
 			db[view.fileName()] = {'counter': 1, 'date': now}
-			
+
 		myfile = open(self.filename, "w")
 		pickle.dump(db, myfile)
 		myfile.close()
-		
+
 		# if the DB exceeds 128kb, trim it
 		file_stats = os.stat(self.filename)
 		file_size = file_stats[stat.ST_SIZE];
 		if file_size >= 128 * 1024**2:
 			self.trimDB()
-			
-	def getDescription(self, item, counter, date):		
+
+	def getDescription(self, item, counter, date):
 		delta = datetime.datetime.today() - date
 		days = delta.days
 		minutes = delta.seconds / 60
 		hours = minutes / 60
 		if days == 0 and hours == 0:
 			age = "%2d mins " % (minutes,)
-		elif days == 0:		
+		elif days == 0:
 			age = "%2d hours" % (hours,)
 		else:
 			age = "%2d days " % (days,)
-		
+
 		return "%2dx, %s - %s" % (counter, age, item)
-		
+
 	def run(self, window, args):
 		db = self.getDB();
-			
+
 		latest       = sorted(db.keys(), key=lambda x:db[x]['date'   ], reverse=True)
 		mostFrequent = sorted(db.keys(), key=lambda x:db[x]['counter'], reverse=True)
 		files = sorted(
 			set(mostFrequent[:10] + latest[:10]),
 			key=lambda x:db[x]['date'],
 			reverse=True)
-		
-		display = map(lambda x: self.getDescription(x, db[x]['counter'], db[x]['date']), files)
 
+		# remove unavailable files (i.e. deleted, on a removable drive, etc)
+		files = [f for f in files if f and os.path.isfile(f)]
+
+		display = map(lambda x: self.getDescription(x, db[x]['counter'], db[x]['date']), files)
+		# pp(files)
 		window.showQuickPanel("", "open", files, display, sublime.QUICK_PANEL_FILES)
