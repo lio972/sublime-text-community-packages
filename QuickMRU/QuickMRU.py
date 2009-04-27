0419e53e32aa9ca66ca42c4aa2828d495e15e0ef
Sami.Pirbay <Sami.Pirbay@159fff5a-be49-0410-a0ae-73795322737e>
2009-04-27 17:55:42 +0000

    Optimized DB to be loaded only when necessary
    Now using UTC time to be timezone independent

--- QuickMRU/QuickMRU.py
+++ QuickMRU/QuickMRU.py
@@ -1,7 +1,7 @@
 import sublime, sublimeplugin
 import os, os.path, sys, stat
 import cPickle as pickle
-import datetime
+from datetime import datetime
 from pprint import pprint as pp
 
 class QuickMRUCommand(sublimeplugin.WindowCommand):
@@ -9,71 +9,89 @@ class QuickMRUCommand(sublimeplugin.WindowCommand):
 	Shows a quick panel with the most recently closed files and the	files closed most often.
 	"""
 
-	filename = sublime.packagesPath() + "/QuickMRU/QuickMRU.db" # is there a better way to get the plugin's folder?
+	dbfilename = sublime.packagesPath() + "/QuickMRU/QuickMRU.db" # is there a better way to get the plugin's folder?
 
-	def getDB(self):
+	__db = None
+
+	def _get_db(self):
+		if(self.__db is None): self.reloadDB()
+		return self.__db
+
+	def _set_db(self, value):
+		self.__db = value
+		self.dumpDB()
+
+	db = property(_get_db, _set_db)
+
+	def reloadDB(self):
+		# print "QuickMRU: loading DB from disk"
 		try:
-			myfile = open(self.filename, "r")
-			db = pickle.load(myfile)
+			myfile = open(self.dbfilename, "r")
+			self.__db = pickle.load(myfile)
 			myfile.close()
 		except IOError:
-			db = {}
-		return db
+			print "QuickMRU: DB not found, creating new one"
+			self.__db = {}
+
+	def dumpDB(self):
+		# print "QuickMRU: dumping DB to disk"
+		myfile = open(self.dbfilename, "w")
+		pickle.dump(self.db, myfile)
+		myfile.close()
 
 	def trimDB(self):
-		db = self.getDB()
+		print "QuickMRU: Trimming DB"
+		db = self.db
 		latest       = sorted(db.keys(), key=lambda x:db[x]['date'   ], reverse=True)
 		mostFrequent = sorted(db.keys(), key=lambda x:db[x]['counter'], reverse=True)
 		files = set(mostFrequent[:100] + latest[:100])
 		newDB = {}
 		for x in files:
 			newDB[x]=db[x]
-
 		del newDB[None]
 
-		myfile = open(self.filename, "w")
-		pickle.dump(newDB, myfile)
-		myfile.close()
+		self.db = newDB
+
 
 	def onClose(self, view):
 		if not view.fileName():
 			return
 
-		db = self.getDB();
-
-		now = datetime.datetime.today();
-		if(view.fileName() in db):
-			db[view.fileName()]['counter'] += 1
-			db[view.fileName()]['date'] = now
+		now = datetime.utcnow()
+		if(view.fileName() in self.db):
+			self.db[view.fileName()]['counter'] += 1
+			self.db[view.fileName()]['date'] = now
 		else:
-			db[view.fileName()] = {'counter': 1, 'date': now}
+			self.db[view.fileName()] = {'counter': 1, 'date': now}
 
-		myfile = open(self.filename, "w")
-		pickle.dump(db, myfile)
-		myfile.close()
+		self.dumpDB()
 
 		# if the DB exceeds 128kb, trim it
-		file_stats = os.stat(self.filename)
-		file_size = file_stats[stat.ST_SIZE];
+		file_stats = os.stat(self.dbfilename)
+		file_size = file_stats[stat.ST_SIZE]
 		if file_size >= 128 * 1024**2:
 			self.trimDB()
 
-	def getDescription(self, item, counter, date):
-		delta = datetime.datetime.today() - date
+	def getDesc(self, item):
+		counter = self.db[item]['counter']
+		date    = self.db[item]['date']
+
+		delta = datetime.utcnow() - date
 		days = delta.days
 		minutes = delta.seconds / 60
 		hours = minutes / 60
+
 		if days == 0 and hours == 0:
-			age = "%2d mins " % (minutes,)
+			age = "%2d mins " % minutes
 		elif days == 0:
-			age = "%2d hours" % (hours,)
+			age = "%2d hours" % hours
 		else:
-			age = "%2d days " % (days,)
+			age = "%2d days " % days
 
 		return "%2dx, %s - %s" % (counter, age, item)
 
 	def run(self, window, args):
-		db = self.getDB();
+		db = self.db
 
 		latest       = sorted(db.keys(), key=lambda x:db[x]['date'   ], reverse=True)
 		mostFrequent = sorted(db.keys(), key=lambda x:db[x]['counter'], reverse=True)
@@ -85,6 +103,7 @@ class QuickMRUCommand(sublimeplugin.WindowCommand):
 		# remove unavailable files (i.e. deleted, on a removable drive, etc)
 		files = [f for f in files if f and os.path.isfile(f)]
 
-		display = map(lambda x: self.getDescription(x, db[x]['counter'], db[x]['date']), files)
+		display = [self.getDesc(f) for f in files]
 		# pp(files)
+		# print len(db.keys())
 		window.showQuickPanel("", "open", files, display, sublime.QUICK_PANEL_FILES)
