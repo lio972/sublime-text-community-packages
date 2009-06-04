f4a7d2d5c5eaef6101c102ac0304fc4df7e94d6a
ndudfield <ndudfield@159fff5a-be49-0410-a0ae-73795322737e>
2009-06-04 13:20:02 +0000

    Not sure if this is in working state LOL. Committing for people wanting to have a play.

--- IPython/ipython_completion.py
+++ IPython/ipython_completion.py
@@ -1,6 +1,6 @@
 ################################################################################
-# Std Libs 
-            
+# Std Libs
+
 from __future__ import with_statement
 
 import functools
@@ -21,7 +21,7 @@ import sublimeplugin
 # User Libs
 
 from pywinauto_ipython import launch_ipython
-
+from windows import activateApp
 completion_server = join(sublime.packagesPath(), 'IPython', 'server')
 
 ################################################################################
@@ -44,11 +44,13 @@ def contig(view, pt):
 
 class IPythonBridge(sublimeplugin.TextCommand):
     pywinauto_ip   = None
-    
+
     def __init__(self):
         self.reset()
+        self.installAutoCompletion()
 
     def reset(self):
+        self.connected = False
         self.IP = Pyro.core.getProxyForURI("PYROLOC://localhost:7380/IPython")
         self.history_length = 0
         self.history_pos    = -1
@@ -56,16 +58,18 @@ class IPythonBridge(sublimeplugin.TextCommand):
     def statusMessage(self, msg):
         sublime.statusMessage("%s: %s" % (self.__class__.__name__, msg))
 
+    def installAutoCompletion(self):
+        from AutoComplete import AutoCompleteCommand
+        AutoCompleteCommand.completionCallbacks['ipython'] = self.auto_completion
+
     def launchIPython(self, view):
+        # TODO attach to ipython
         if not self.pywinauto_ip:
             self.pywinauto_ip = launch_ipython(completion_server)
         else:
             try:
                 self.pywinauto_ip.TypeKeys('', 0)
             except Exception:
-                # print type(self.pywinauto_ip)
-                # print self.pywinauto_ip.criteria
-                # print dir(self.pywinauto_ip.app)
                 try:
                     self.pywinauto_ip = launch_ipython(completion_server)
                 except Exception, e:
@@ -74,21 +78,21 @@ class IPythonBridge(sublimeplugin.TextCommand):
     def historyLines(self, view, direction):
         history = self.IP.input_hist()
         history_length = len(history)
-        
+
         if history_length != self.history_length:
             self.history_length = history_length
             self.history_pos = -1
         else:
             if direction == 'up':     self.history_pos -= 1
             else:                     self.history_pos += 1
-        
+
         index = (self.history_pos % (history_length-1)) + 1
         history_line = history[index]
-        
+
         indexed_line = 'In[%s] %s' % (index, history_line)
         sel = view.sel()[0]
         sublime.statusMessage(indexed_line)
-        
+
         view.replace(sel, history_line)
 
     def pushLines(self, view):
@@ -99,22 +103,28 @@ class IPythonBridge(sublimeplugin.TextCommand):
         to_push = textwrap.dedent(''.join(lines))
         self.IP.push(to_push)
         self.statusMessage('%s lines pushed to IPython' % len(lines))
-    
-    def autoComplete(self, view):
-        current_word = (view.substr(contig(view, view.sel()[0].end())))
-        current_line = view.substr(view.line(view.sel()[0]))
-        
-        completion, matches = self.IP.complete(current_word, current_line)
 
-        if matches:
-            window = sublime.activeWindow()
-            window.showQuickPanel("", 'insertIPythonCompletion', matches )
-        else:
-            view.runCommand('autoComplete')
+    def auto_completion(self, view, pos, prefix, completions):
+        if view.matchSelector(0, 'source.python') and self.connected:
+            contiguous_region = contig(view, pos)
+            contiguous = view.substr(contiguous_region)
+            current_line = view.substr(view.line(view.sel()[0]))
+    
+            completion, matches = self.IP.complete(contiguous, current_line)
+            prefix_length_diff = len(contiguous) - len(prefix)
+    
+            matches = sorted( (s[prefix_length_diff:] for s in matches), 
+                              reverse=True )
+
+            for c in (m for m in matches if m not in completions):
+                completions.append(c)
+
+        return completions
 
     def run(self, view, args):
         try:
             getattr(self, args[0])(view, *args[1:])
+            self.connected = True
         except Exception, e:
             self.statusMessage(e)
             self.reset()
@@ -126,5 +136,5 @@ class InsertIPythonCompletion(sublimeplugin.TextCommand):
     def run(self, view, args):
         v = sublime.activeWindow().activeView()
         v.replace(contig(v, v.sel()[0].end()), args[0])
-        
+
 ################################################################################
