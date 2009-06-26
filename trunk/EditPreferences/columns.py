938252f7a001a16ea43a87c9935f1a04adcb5bd5
ndudfield <ndudfield@159fff5a-be49-0410-a0ae-73795322737e>
2009-06-26 03:48:34 +0000

    EditPreferences: Back-ported and in-lined newer version of columns.py; Back-ported insertBindingRepr command. Updated README.txt.

--- trunk/EditPreferences/columns.py
+++ trunk/EditPreferences/columns.py
@@ -2,16 +2,17 @@
 
 import os
 import math
+import random
 
 ################################### SETTINGS ###################################
 
 SCREEN_WIDTH = 150      # char width of quick panel (select then copy => status)
 
-CELL_ALIGN = unicode.ljust                           # rjust for right alignment 
-                            
+CELL_ALIGN = unicode.ljust                           # rjust for right alignment
+
 STRIP = unicode.rstrip                                    # see ellpsis function
 
-POWARRAH = -0.5 
+POWARRAH = -0.5
 
 CELL_PADDING = 2
 
@@ -19,49 +20,64 @@ DECODE = lambda s: s.decode('utf8', 'ignore')
 
 ############################### COLUMN RENDERING ###############################
 
-def find_widths(columns, total_width=SCREEN_WIDTH, cell_padding=CELL_PADDING):
-    O = int(len(columns[0]) / 10.0)
+def normed_column_widths(columns):
+    outliers = int(columns and len(columns[0] or 0) / 10.0)
 
     # Find the average, discarding some outliers
-    seq =  [ (sum(len(element) for element in L) /len(L)) for L in   
-             [sorted(S)[O:len(S)-O] for S in columns] ]
+    return  [ (sum(len(e) for e in col) / (len(col) or 1)) for col in
+             [sorted(col)[outliers:len(col)-outliers] for col in columns] ]
+
+def yield_widths(columns, total_width=SCREEN_WIDTH, cell_padding=CELL_PADDING):
+    colw = normed_column_widths(columns)
 
     n = len(columns)
     screen_width = total_width - n * cell_padding  # padded by render_rows func
 
     # power scaling; smaller columns get bigger share of leftovers
-    seq = [x * (float((x ** POWARRAH) * screen_width)/sum(seq)) for x in seq]
+    scaling = [ x * (float( ( x ** POWARRAH ) * screen_width)/sum(colw))
+                if x else 0
+                for x in colw ]
 
     # Distribute the leftovers
-    column_widths = [ int( x*screen_width/sum(seq) )  for x in seq]
+    column_widths = [ int( s * screen_width/sum(scaling) ) for s in scaling]
 
     for i, col in enumerate(columns):
         # Pop off a column width
         colw = column_widths.pop(0)
         maxw = max(len(l) for l in col)
-        width = min(maxw, colw)
 
-        # If there is leftovers (column assigned more width than its largest
-        # cell) then redistribute the wealth amongst the remaining columns
-        # As cell padding has already been accounted set cell padding to 0
-        # for recursive call
+        width = min(maxw, colw)
 
         if maxw < colw:
             remaining_cols = columns[i+1:]
             remaining_width = abs( maxw - colw ) + sum(column_widths)
             if remaining_cols:
-                column_widths = list ( 
-                    find_widths(remaining_cols, remaining_width, 0) )
+                column_widths = list (
+                    yield_widths(remaining_cols, remaining_width, 0) )
 
         yield width
 
+def find_widths(columns):
+    lengths = normed_column_widths(columns)
+    tosort = sorted ((
+        (i, lengths[i]) for i in xrange(len(columns)) ), key=lambda t: t[1] )
+
+    widths = list(yield_widths([columns[i] for i in [s[0] for s in tosort] ]))
+
+    for i, w in enumerate(widths):
+        lengths[ tosort[i][0] ] = w
+
+    return lengths
+
 def pad_columns(columns, align=CELL_ALIGN):
     padded = []
-    
+
     for i, (col, width) in enumerate(zip(columns, find_widths(columns))):
         def ellipsis(s):
             # If it is the last column just let it overhang, no sense in cutting
-            return ( s if (len(s) <= width or i+1 == len(columns)) 
+            return ( s if (len(s) <= width
+                        or i+1 == len(columns)
+                    )
                      else s[:width-3] + ' ..')
 
         # rstrip each cell
@@ -79,7 +95,7 @@ def rows_2_columns(rows):
 def rendered_rows(rows, pl=0, pr=CELL_PADDING):
     # render each row as unicode with padded cells to stop them getting lonely
     pad = lambda r: [(pl*' ') + c + (pr*' ') for c in r]
-    return [''.join(pad(r)) for r in rows]
+    return [''.join(pad(r)).decode('utf8', 'ignore') for r in rows]
 
 def remove_common_prefix(col):
     common_prefix = os.path.commonprefix(col)
@@ -91,5 +107,30 @@ def format_for_display(args, cols=(), paths=()):
     columns = [ remove_common_prefix(c) if i in paths else c
                 for i, c in enumerate(columns) ]
     return rendered_rows(columns_2_rows(pad_columns(columns)))
+
+############################### FUCKAROUND TESTS ###############################
+
+def test():
+    cols = (40, 80)
+    col2 = (5,  10)
+    col3 = (5,  10)
+    col4 = (5,  10)
+
+    args = []
+
+    for i in range(100):
+        args.append ((
+            random.randint(*cols) * 'a',
+            random.randint(*col2) * 'b',
+            random.randint(*col3) * 'c',
+            random.randint(*col4) * 'c',
+        ))
+
+    args = format_for_display(args)
+    print '\n'.join(args)
+    print max(len(l.rstrip()) for l in args)
+
+if __name__ == '__main__':
+    test()
 
 ################################################################################
