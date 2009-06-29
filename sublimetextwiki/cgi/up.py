4b90af26a83f483274c3d814352ba7765ad05dd5
mindfiresoftware <mindfiresoftware@159fff5a-be49-0410-a0ae-73795322737e>
2009-06-29 14:04:35 +0000

    updated build process to log update messages sent from Google

--- sublimetextwiki/cgi/up.py
+++ sublimetextwiki/cgi/up.py
@@ -2,8 +2,32 @@
 
 print "Content-type: text/plain\n\n"
 
-import util, os, subprocess
+import util, os, subprocess, cgi
+import datetime, os, util
+import cgitb
+cgitb.enable()
+
+
+revision = "unknown"
+project  = "unknown"
+
+form = cgi.FieldStorage()
+
+if form.has_key('revision'):
+  revision = form['revision'].value
+  
+if form.has_key('project'):
+  project = form['project'].value
+  
+msg = "Updating project %s, revision %s, at %s\n" % (project, revision, datetime.datetime.now())
+print msg
+
+# log the update attempt.
+log = os.path.join(util.site, 'svnlog.txt')
+f = file(log, 'a')
+f.write(msg)
+f.close()
+
 up = os.path.join(util.site, 'up')
 print up
 subprocess.call(up)
-#util.run([up], util.site)
