c391aa19aef0217cad1752d67ac3caad539dd2b1
msiniscalchi <msiniscalchi@159fff5a-be49-0410-a0ae-73795322737e>
2009-09-25 21:16:12 +0000

    (LaTeX) Correct handling of > 16 references, braces in prefix

--- trunk/LaTeX/texRef.py
+++ trunk/LaTeX/texRef.py
@@ -2,6 +2,8 @@ import sublime, sublimeplugin, os, os.path, re
 
 # References and citations
 
+done = False
+
 class TexRefCommand(sublimeplugin.TextCommand):
 	def run(self, view, args):
 		print "entering texRef"
@@ -13,17 +15,29 @@ class TexRefCommand(sublimeplugin.TextCommand):
 		prefix = view.substr(view.word(point)).strip()
 		print currsel, point, prefix,len(prefix)
 		completions = []
-		view.findAll('\\label\{(.*)\}',0,'\\1',completions)
-		print completions
-		if not prefix in ["", "{}"]:
+		view.findAll('\\label\{([^\{]*)\}',0,'\\1',completions)
+#		print completions
+#		print "%d labels" % (len(completions),)
+		if not prefix in ["", "{}", "{", "}"]:
 			fcompletions = [comp for comp in completions if prefix in comp]
 		else:
-			prefix = "" # in case it's {}
+			prefix = "" # in case it's {} or {
 			fcompletions = completions
-		view.showCompletions(point, prefix, fcompletions)
-		
-		# def onSelect(i):
-		# 	view.replace(currsel, "\\ref{" + fcompletions[i] + "}")
-		
-		# view.window().showSelectPanel(fcompletions, onSelect, None, 0)
-		# print "done"
+		# The drop-down completion menu contains at most 16 items, so
+		# show selection panel if we have more.
+		print prefix, len(fcompletions)
+		if len(fcompletions) <= 16:
+			view.showCompletions(point, prefix, fcompletions)
+		else:
+			def onSelect(i):
+				# if we had an actual prefix, replace it with the label,
+				# otherwise insert the label.
+				# FIXME like TextMate, if you give e.g. thm:so as prefix
+				# you may get thm:thm:something
+				if prefix not in ["", "{}", "{", "}"]:
+					view.replace(currword, fcompletions[i])
+				else:
+					view.insert(point, fcompletions[i])
+			view.window().showSelectPanel(fcompletions, onSelect, None, 0)
+		print "done"
+
