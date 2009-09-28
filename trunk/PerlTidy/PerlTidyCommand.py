f467deaadc1b3592a031906903f1e67f145a4253
Sami.Pirbay <Sami.Pirbay@159fff5a-be49-0410-a0ae-73795322737e>
2009-09-28 20:45:26 +0000

    Added capability to run current file through Perl and see results in scratch buffer.

--- trunk/PerlTidy/PerlTidyCommand.py
+++ trunk/PerlTidy/PerlTidyCommand.py
@@ -1,5 +1,4 @@
 import sublime, sublimeplugin
-
 import subprocess, os.path
 
 class PerlTidyCommand(sublimeplugin.TextCommand):
@@ -16,26 +15,38 @@ class PerlTidyCommand(sublimeplugin.TextCommand):
 
 	# Default path to PerlTidy, change this to your own PerlTidy installation if you have one
 	DefaultPerlTidy = 'C:/Perl/site/bin/perltidy'
-	
+
 	# Pretty printing parameters, customize to your liking
 	PrettyPrintingParams = [
-		"-sbl",   # open sub braces on new line
-		"-bbt=1", # add only some spaces in single-line curly braces
-		"-pt=2",  # dont add spaces inside parenthesis
-		"-nbbc",  # don't add blank lines before comments
-		"-l=100", # wrap at column 100
+		"-sbl",     # open sub braces on new line
+		"-bbt=1",   # add only some spaces in single-line curly braces
+		"-pt=2",    # dont add spaces inside parenthesis
+		"-nbbc",    # don't add blank lines before comments
+		"-l=100",   # wrap at column 100
+		"-ole=unix" # unix line endings
 	]
 
 	def run(self, view, args):
-		if(view.sel()[0].empty()):
+		if view.sel()[0].empty():
 			# nothing selected: process the entire file
-			self.tidyRegion(view, sublime.Region(0L, view.size()))
+			r = sublime.Region(0L, view.size())
 		else:
 			# process only selected region
-			self.tidyRegion(view, view.line(view.sel()[0]))
+			r = view.line(view.sel()[0])
+
+		if "runPerl" in args:
+			# just run the selected text through Perl for quick tests
+			self.runPerl(view, r)
+		else:
+			# Run PerlTidy, and restore approximate cursor location
+			cursor = view.sel()[0];
+			self.tidyRegion(view, r)
+			if cursor.empty():
+				view.sel().add(cursor)
+				view.sel().subtract(view.sel()[1])
 
 	def tidyRegion(self, view, region):
-		if(os.path.isfile(self.DefaultPerlTidy)):
+		if os.path.isfile(self.DefaultPerlTidy):
 			# use installed PerlTidy
 			cmd = [self.DefaultPerlTidy]
 			# print "using installed PerlTidy", cmd
@@ -64,9 +75,33 @@ class PerlTidyCommand(sublimeplugin.TextCommand):
 		view.replace(region, output)
 
 		if error:
-			print "\nPerlTidy error:\n", error
-			sublime.messageBox("PerlTidy: Error detected, check output panel for more information")
+			results = view.window().newFile()
+			results.setScratch(True)
+			results.setName("PerlTidy error output")
+			results.insert(0, error)
+
+	def runPerl(self, view, region):
+		"""
+		Just run the code through Perl for quick tests, and show output
+		"""
+		p = subprocess.Popen(
+			"perl",
+			shell   = True,
+			bufsize = -1,
+			stdout  = subprocess.PIPE,
+			stderr  = subprocess.PIPE,
+			stdin   = subprocess.PIPE)
+
+		output, error = p.communicate(view.substr(region))
+
+		results = view.window().newFile()
+		results.setScratch(True)
+		results.setName("Perl output")
+		
+		results.insert(-1, output + "\n")
+		if error:
+			results.insert(-1, "\nSTDERR:\n" + error)
 
 	def isEnabled(self, view, args):
-		# enabled for Perl files, with at most 1 selection region
-		return len(view.sel()) == 1 and view.options().getString("syntax") == "Packages/Perl/Perl.tmLanguage"
+		# enabled for Perl and Plain text files, with at most 1 selection region
+		return len(view.sel()) == 1 and view.options().getString("syntax") in [u"Packages/Perl/Perl.tmLanguage", u"Packages/Text/Plain text.tmLanguage"]
