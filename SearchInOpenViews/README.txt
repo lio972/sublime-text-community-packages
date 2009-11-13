c864c921bc1c5f41df7ea28e280a335f41301225
vimperator <vimperator@159fff5a-be49-0410-a0ae-73795322737e>
2009-11-13 10:54:24 +0000

    SearchInopenViews v2 - uses input panel and shows exact term searched

--- SearchInOpenViews/README.txt
+++ SearchInOpenViews/README.txt
@@ -1,5 +1,12 @@
-Will Search Open Views
+# Search In Open Views 
 
-Will source search `input` string from either current selection if not empty or the clipboard
+allows you to search any term on all your open files (views)
 
-*    `ctrl+alt+f` :  `search`
+## Key Binding
+
+	<ctrl+alt+f> pop the special find panel
+	
+## Changes from last version
+
+- revised the whole selection process, now uses an input bar. 
+- after selecting the desired file/loaction the searched term will be marked rather than the whole line.
---
+++ SearchInOpenViews/SearchInOpenViews.py
@@ -0,0 +1,103 @@
+import sublime, sublimeplugin
+import functools
+
+#------------------------------------------------------------------
+#------------------------------------------------------------------
+class SearchInOpenViewsCommand(sublimeplugin.TextCommand):
+   def run(self, view, args):
+      view_list = self.get_view_list(view)
+      
+      first_selection = view.sel()[0]
+      if first_selection.begin() - first_selection.end() != 0:
+         init_pattern = view.substr(first_selection)
+      else:
+         init_pattern = ""
+         
+      view.window().showInputPanel("Search in open files:", init_pattern,
+         functools.partial(self.on_start_searching, view, view_list),
+         self.on_change, self.on_cancel)
+
+   #------------------------------------------------------------------
+   def on_start_searching(self, view, view_list, pattern):
+      result = []
+            
+      for v in view_list:
+         result += self.search_term(v, pattern)
+      
+      display = []
+      for (file, line, txt, region) in result:
+         display.append(file + ":" + str(line) + ": " + txt)
+                         
+      flags = sublime.QUICK_PANEL_FILES
+      view.window().showSelectPanel(display,
+         functools.partial(self.open_selection, view, view_list, result), None,
+         flags)
+      
+   def on_change(self, input):
+      pass
+   
+   def on_cancel(self):
+      pass   
+      
+   #------------------------------------------------------------------
+   def search_term(self, view, pattern):
+      result = []
+      next_region = view.line(0) # start from 0
+      while next_region is not False:
+         region = view.find(pattern, next_region.begin(), 0)
+         if region is not None:
+            (row, col) = view.rowcol(region.begin())
+            
+            file_name = view.fileName()
+            line_number = row+1
+            line_text = view.substr(view.line(region))
+            res_tuple = (file_name, line_number, line_text, region)
+            
+            result.append(res_tuple)
+            next_region = self.advance_line(view, region)
+         else:
+            break
+      return result
+   
+   #------------------------------------------------------------------
+   def get_view_list(self, view):
+      unique_views = []
+      file_names = []
+      for v in view.window().views():
+         # avoid re-searching same view (if opened twice)
+         if not v.fileName() in unique_views:
+            unique_views.append(v)
+            file_names.append(v.fileName())
+      return unique_views
+   
+   #------------------------------------------------------------------
+   def isEnabled(self, view, args):
+      if view.fileName() is not None:
+         return True
+      else:
+         return False
+   
+   #------------------------------------------------------------------
+   def advance_line(self, view, last_region):
+      (row, col) = view.rowcol(last_region.begin())
+      next_point = view.textPoint(row+1, 0)
+      next_region = view.line(next_point)
+      
+      if next_region.begin() > last_region.end():
+         return next_region
+      else:
+         return False
+
+#------------------------------------------------------------------
+   def open_selection(self, view, view_list, results_list, index):
+      (file, row, text, region) = results_list[index]
+      
+      for v in view_list:
+         if file == v.fileName():
+            view.window().focusView(v)
+            # reset selection
+            view_selection = v.sel()
+            view_selection.clear()
+            # add new selection
+            view_selection.add(region)
+            v.show(region)
