739e89a0b8b751810fafac77072c66d06368b35b
msiniscalchi <msiniscalchi@159fff5a-be49-0410-a0ae-73795322737e>
2010-02-25 23:31:35 +0000

    (LaTeX) Refactored texSections.py, added detection of Beamer frames

--- LaTeX/texSections.py
+++ LaTeX/texSections.py
@@ -9,47 +9,49 @@ spaces = {'part' : '', 'chapter' : '  ', 'section' : '    ',
 class TexSectionsCommand(sublimeplugin.TextCommand):
 	def run(self, view, args):
 		# First get raw \section{xxx} lines
-		# Note that these may be followed by \label{xxx}
-		sections = []
-		view.findAll(r'\\(part|chapter|(?:sub)*section)\{(.+)\}',0,r"\\\1\{\2\}",sections)
-		# Now filter out possible ending \label{xxx}'s
-		def killLabel(s):
-			#print "kill " + s
-			# Assume first { has been skipped: e.g. "Title of section}"
-			# Will return text except for final }
-			n = 1
-			news = ""
-			for c in s:
-				#print c,n
-				if c == '}':
-					n -= 1
-					if n == 0:
-						#print news
-						return news
-				elif c == '{':
-					n += 1
-				news += c
-				#print news
-		#print killLabel("hel\label{test}p} \label{test2}")
-		#print sections
-		# len(key)+2 to skip initial \ and first {
-		fsections = [spaces[key] + killLabel(sec[len(key)+2:])
-					 for sec in sections 
-					 # 1:1+len(key) to skip initial \
-					 for key in spaces.keys() if sec[1:1+len(key)] == key]
-
-		#print fsections
+		# Capture the entire line beginning with our pattern, do processing later
+		secRegions = view.findAll(r'^\\(begin\{frame\}|part|chapter|(?:sub)*section).*$')
+		# Remove \section, etc and only leave spaces and titles
+		# Handle frames separately
+		# For sections, match \ followed by type followed by * or {, then
+		# match everything. This captures the last }, which we'll remove
+		secRe = re.compile(r'\\([^{*]+)\*?\{(.*)') # \, then anything up to a * or a {
+		# Also, we need to remove \label{} statements
+		labelRe = re.compile(r'\\label\{.*\}')
+		# And also remove comments at the end of the line
+		commentRe = re.compile(r'%.*$')
+		# This is to match frames
+		# Here we match the \begin{frame} command, with the optional [...]
+		# and capture the rest of the line for further processing
+		# TODO: find a way to capture \frametitle's on a different line
+		frameRe = re.compile(r'\\begin\{frame\}(?:\[[^\]]\])?(.*$)')
+		frameTitleRe = re.compile(r'\{(.*)\}')
+		def prettify(s):
+			s = commentRe.sub('',s).strip() # kill comments at the end of the line, blanks
+			s = labelRe.sub('',s).strip() # kill label statements
+			frameMatch = frameRe.match(s)
+			if frameMatch:
+				frame = frameMatch.group(1)
+				frameTitleMatch = frameTitleRe.search(frame)
+				if frameTitleMatch:
+					return "frame: " + frameTitleMatch.group(1)
+				else:
+					return "frame: (untitled)"
+			else:
+				m = secRe.match(s)
+				#print m.group(1,2)
+				secTitle = m.group(2)
+				if secTitle[-1]=='}':
+					secTitle = secTitle[:-1]
+				return spaces[m.group(1)]+secTitle
+		prettySecs = [prettify(view.substr(reg)) for reg in secRegions]
+		
 		def onSelect(i):
-			seci = sections[i]
-			#print seci
-			#note flag sublime.LITERAL, so "{", "(", "\" are all OK
-			linept = view.find(seci,0,sublime.LITERAL).end()
+			#print view.substr(secRegions[i])
+			view.show(secRegions[i])
 			s = view.sel() # RegionSet
 			s.clear()
-			s.add(sublime.Region(linept,linept))
-			view.show(linept)
+			s.add(secRegions[i])
 			view.runCommand("moveTo bol")
 
-		view.window().showSelectPanel(fsections, onSelect, None, 0)
-		#print "done2"
-
+		view.window().showSelectPanel(prettySecs, onSelect, None, 0)
