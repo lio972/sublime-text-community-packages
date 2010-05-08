d386a9ae08523f96b63fcc2917b6b0b8c068aef7
barney.williams <barney.williams@159fff5a-be49-0410-a0ae-73795322737e>
2010-05-08 19:26:11 +0000

    Added focus control (alt) and moved view relocation to (alt+shift) Updated docs

--- trunk/ViewNav/MoveToBuffer.py
+++ trunk/ViewNav/MoveToBuffer.py
@@ -1,21 +1,33 @@
+# This plugin allows you to more easily manage pane focus and view location
+# when in a multiple group (pane) layout
+#
+# args[0] value:    action
+# ---------------------------------
+#         move:		move the current view (buffer) to a different group (pane)
+#		  focus:	makes a different group (pane) active
+#
+# args[1] value:    destination
+# ---------------------------------
+#        (default)  next group
+#         right: 	next group
+#         down:  	next group
+#         next:  	next group
+#         left:     previous group 
+#         up:       previous group
+#         previous: previous group
+
 import sublime, sublimeplugin
 
-# This plugin allows you to more easily it will move the active view to the
-# next/previous group.
-#
-# args[0] value: destination
-#         right: 	next buffer
-#         down:  	next buffer
-#         next:  	next buffer
-#         left:     previous buffer 
-#         up:       previous buffer
-#         previous: previous buffer
- 
 class MoveToBufferCommand(sublimeplugin.TextCommand):
 	def run(self, view, args):
 		window = view.window()
-		if args[0] == "right" or args[0] == "down" or args[0] == "next":
-			groupIdx = (window.activeGroup() + 1) % window.numGroups()
-		else:
+		
+		if args[1] == "left" or args[1] == "up" or args[1] == "previous":
 			groupIdx = (window.activeGroup() - 1) % window.numGroups()
-		window.setViewPosition(view, groupIdx, 100000) # moves to end of existing tabs
+		else:
+			groupIdx = (window.activeGroup() + 1) % window.numGroups()
+			
+		if args[0] == "move":
+			window.setViewPosition(view, groupIdx, 100000) # moves to end of existing tabs
+		else:
+			window.focusView(window.activeViewInGroup(groupIdx))
