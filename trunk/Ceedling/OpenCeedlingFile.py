72d3510dd3f925390ffd27638eae949b90cb5785
barney.williams <barney.williams@159fff5a-be49-0410-a0ae-73795322737e>
2010-08-14 01:47:35 +0000

    Added other miscellaneous bindings. Fixed view activation quirk.

--- trunk/Ceedling/OpenCeedlingFile.py
+++ trunk/Ceedling/OpenCeedlingFile.py
@@ -5,10 +5,12 @@ import re
 class OpenCeedlingFileCommand(sublimeplugin.TextCommand):
 
     def run(self, view, args):
+        self.views = []
         current_file_path = view.fileName()
         window = view.window()
+        option = args[0]
         
-        if args[0] == 'config':
+        if option == 'config':
             config_matcher = re.compile(r"(\\\\)?(project|test)\.yml$")
             self.open_project_file(config_matcher, window)
         
@@ -22,23 +24,26 @@ class OpenCeedlingFileCommand(sublimeplugin.TextCommand):
             header_matcher = re.compile("\\\\" + base_name + "\.h$")
             test_matcher   = re.compile("\\\\test_" + base_name + "\.c$")
             
-            if args[0] == 'next':
+            if option == 'next':
                 if re.match("test_", current_file):
                     self.open_project_file(source_matcher, window)
                 elif re.search(r"\.c", current_file):
                     self.open_project_file(header_matcher, window)
                 elif re.search(r"\.h", current_file):
                     self.open_project_file(test_matcher, window)
-            elif args[0] == 'source':
+            elif option == 'source':
                 self.open_project_file(source_matcher, window)
-            elif args[0] == 'test':
+            elif option == 'test':
                 self.open_project_file(test_matcher, window)
-            elif args[0] == 'header':
+            elif option == 'header':
                 self.open_project_file(header_matcher, window)
-            elif args[0] == 'test_and_source':
+            elif option == 'test_and_source':
                 window.runCommand('layoutDoubleVert')
                 self.open_project_file(test_matcher, window, 0)
                 self.open_project_file(source_matcher, window, 1)
+            
+        for v in self.views:
+            window.focusView(v)
                 
     def open_project_file(self, file_matcher, window, auto_set_view=-1):
         proj_files = window.project().mountPoints()[0]['files']
@@ -47,5 +52,5 @@ class OpenCeedlingFileCommand(sublimeplugin.TextCommand):
                 file_view = window.openFile(f)
                 if auto_set_view >= 0: # don't set the view unless specified
                     window.setViewPosition(file_view, auto_set_view, 0)
-                window.focusView(file_view)
+                self.views.append(file_view)
                 print("Opened " + f)
